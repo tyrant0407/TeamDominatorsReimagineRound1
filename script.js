a4ecd7d9e411fde0b65b49ab3e53f305e2b2dbd9
tyrant0407 <amankushwaha1331@gmail.com>
2024-07-02 21:47:25 +0530

    MenuAdded

--- script.js
+++ script.js
@@ -17,13 +17,26 @@ function loadingTimer() {
       }
     }, Math.floor(Math.random() * 300));
   
+
+    gsap.to(".RED",{
+      x:-30,
+      duration:1,
+    },"logo")
+    gsap.to(".YELLOW",{
+        x:30,
+      duration:1,
+    },"logo")
+
+    
     var btn = document.querySelector("#timer button");
     btn.addEventListener("click", function () {
       var tl = gsap.timeline();
+
       tl.to(
         ".loader",
         {
           y: "-100%",
+          duration:1,
         },
         "lol"
       );
@@ -34,32 +47,7 @@ function loadingTimer() {
   }
   
   loadingTimer();
-  // gsap.to(".slite1",{
-  //     x:350,
-  //     opacity:0,
-  //     scrollTrigger:{
-  //         trigger:".slite1",
-  //         scroller:"body",
-  //         markers:true,
-  //         start:"top 50%",
-  //         end:"top 20%",
-  //         scrub:"5",
-  
-  //     }
-  // })
-  // gsap.to(".slite2",{
-  //     x:350,
-  
-  //     scrollTrigger:{
-  //         trigger:".slite1",
-  //         scroller:"body",
-  //         markers:true,
-  //         start:"top 45%",
-  //         end:"top 20%",
-  //         scrub:"3",
-  
-  //     }
-  // })
+
   
   var tl = gsap.timeline({
     scrollTrigger: {
@@ -71,11 +59,13 @@ function loadingTimer() {
       scrub: 1,
     },
   });
-  tl.to(".slite1", {
+  tl.to(".slide1", {
     x: 350,
+  duration:1,
+
     opacity: 0,
     scrollTrigger: {
-      trigger: ".slite1",
+      trigger: ".slide1",
       scroller: "body",
       // markers: true,
       start: "top 50%",
@@ -83,11 +73,11 @@ function loadingTimer() {
       scrub: "5",
     },
   });
-  tl.to(".slite2", {
+  tl.to(".slide2", {
     x: 390,
-  
+  duration:1,
     scrollTrigger: {
-      trigger: ".slite1",
+      trigger: ".slide1",
       scroller: "body",
       // markers: true,
       start: "top 45%",
@@ -95,16 +85,17 @@ function loadingTimer() {
       scrub: "3",
     },
   });
-  
+ 
   gsap.to(".box3", {
-    y: 60,
+    y: 62,
     scrollTrigger: {
       trigger: ".page1",
       scroller: "body",
       start: "top -150",
       end: "top -240%",
-  
       scrub: 5,
     },
-  });
+   });
+
+
       
